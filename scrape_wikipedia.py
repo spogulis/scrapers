8dec570ea6783bc56292ce4fce2efe83e46bbcea
Renārs Grebežs <originalchc@gmail.com>
2018-05-15 22:48:02 +0300

    Trying to implement finding all titles and paragraphs

--- scrape_wikipedia.py
+++ scrape_wikipedia.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import textwrap
 #
 # user_input = input("Please enter the webpage to scrape")
 url = "https://en.wikipedia.org/wiki/Atom"
@@ -8,17 +9,24 @@ response = urlopen(url).read() # Reads (returns) webpage and saves it as a var
 soup = BeautifulSoup(response, "lxml")
 
 content = soup.find("div", class_="mw-parser-output")
-p = content.findAll("p", recursive=False)
-# for p in p:
-#     print(p.text)
+# p = content.findAll("p", recursive=False)  #Find all paragraphs in main body
+# title = content.findAll("span", class_="mw-headline")
+
+together = content.findAll(["p", "title"])
 
 filename = "output.txt"
 file_handle = open(filename, "w+", encoding="utf-8")  # Open file for reading/editing
+
+# print(title)
+# for t in title:
+#     print(t.string)
+
 #write wiki output to file
-for p in p:
-    file_handle.write(p.text)
+# for para in p:
+#     file_handle.write(t.string)
+#     file_handle.write(textwrap.fill(para.text, 100))
+#     file_handle.write("\n")
 
-
-
-# print(table)
-# print(soup.html.body.table['class':'infobox'])
+for f in together:
+    print(f.title)
+    print(f.p)
